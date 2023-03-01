3df4b0408e88cb1f477f9a51267975228db6093b
Lutiago <perillaluisa11@gmail.com>
2023-03-01 23:35:10 +0000

    card commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
+const suits = ["♠", "♡", "♢", "♣"];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+function getRandomItem(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function dealCard() {
+  const cardElement = document.getElementById("card");
+  const card = {
+    suit: getRandomItem(suits),
+    value: getRandomItem(values)
+  };
+  cardElement.innerHTML = `
+    <span class="value">${card.value}</span>
+    <span class="suit">${card.suit}</span>
+  `;
+  cardElement.style.backgroundColor = "white";
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.addEventListener("load", dealCard);
+document.getElementById("deal-button").addEventListener("click", dealCard);
